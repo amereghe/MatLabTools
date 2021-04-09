995ad354ea68a93f6b00eba80564a2b2a1fc9fdd
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2021-04-09 15:39:42 +0200

    better error handling

--- operations/GetOPDataFromTables.m
+++ operations/GetOPDataFromTables.m
@@ -3,7 +3,9 @@ function myData=GetOPDataFromTables(FileName,sheetName)
 % used in operations, e.g. table for rampgen or table of LGEN currents.
     tmpFile=dir(FileName);
     if ( length(tmpFile)>1 )
-        error("Path %s is not a single file!",tmpFile);
+        error("Path %s is not a single file!",FileName);
+    elseif ( length(tmpFile)<1 )
+        error("File %s does not exist!",FileName);
     end
     fprintf("acquring data from file %s in folder %s...\n",tmpFile(1).name,tmpFile(1).folder);
     if ( exist('sheetName','var') )
