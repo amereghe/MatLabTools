7107a31401f83264ae906b109f3e3e270fd38aab
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-08-02 15:50:52 +0200

    showing fitted data can handle also only betatronic fits and multiple scans

--- optics/ShowFittedOpticsFunctions.m
+++ optics/ShowFittedOpticsFunctions.m
@@ -1,7 +1,6 @@
-function ShowFittedOpticsFunctions(beta,alpha,emiG,disp,dispP,sigdpp,plane)
+function ShowFittedOpticsFunctions(beta,alpha,emiG,disp,dispP,sigdpp,plane,labels)
 % ShowFittedOpticsFunctions        show the results of the reconstructed
-%                                  optics functions based on the fit vs sigdpp 
-%                                  (scan version)
+%                                  optics functions based on a 6D fit
 %
 % input:
 % - beta [m], alpha [], emiG [pi m rad]: optics functions and geometric
@@ -9,45 +8,80 @@ function ShowFittedOpticsFunctions(beta,alpha,emiG,disp,dispP,sigdpp,plane)
 % - disp [m], dispP [], sigdpp []: dispersion, dispersion prime and sigma_delta_p_over_p
 %              as reconstructed from fits;
 % - plane: current plane being plotted;
-% Nota Bene: beta, alpha, emiG, disp, dispP are arrays as long as sigdpp;
+% - labels (optional): string of labels, identifying different fit cases;
+%   if not given, the passed optics functions are assumed to be reconstructed
+%                 by a fit parameteric in sigdpp; hence, beta, alpha, emiG,
+%                 disp, dispP are arrays as long as sigdpp;
 %
 % the generated figure has 2 rows:
 % - on the first one, there are the betatron optics and emittance;
 % - on the second one, the dispersion ones;
 %
+% more info at:
+%      https://accelconf.web.cern.ch/d99/papers/PT10.pdf
+%
 % See also ShowFittedOrbits.
 
     %% setting up
     figure();
+    if ( exist('labels','var') && size(beta,2)==1 )
+        xVals=1:length(labels);
+        myXlabel="";
+    else
+        xVals=sigdpp;
+        myXlabel="\sigma_{\Deltap/p} []";
+    end
     
     %% first row of plots: beta, alpha and emittance 
     ax1=subplot(2,3,1);
-    plot(sigdpp,beta,'*-');
-    title("\beta"); ylabel("[m]"); xlabel("\sigma_{\Deltap/p} []");
+    plot(xVals,beta,'*-');
+    title("\beta"); ylabel("[m]"); xlabel(myXlabel);
     grid on;
+    if ( exist('labels','var') && size(beta,2)==1 ), xticks(1:length(labels)); xticklabels(labels); xtickangle(45); end
+    if ( size(beta,2)>1 ), legend(labels,'Location','best'); end
     %
     ax2=subplot(2,3,2);
-    plot(sigdpp,alpha,'*-');
-    title("\alpha"); ylabel("[]"); xlabel("\sigma_{\Deltap/p} []");
+    plot(xVals,alpha,'*-');
+    title("\alpha"); ylabel("[]"); xlabel(myXlabel);
     grid on;
+    if ( exist('labels','var') && size(beta,2)==1 ), xticks(1:length(labels)); xticklabels(labels); xtickangle(45); end
+    if ( size(beta,2)>1 ), legend(labels,'Location','best'); end
     % 
     ax3=subplot(2,3,3);
-    plot(sigdpp,emiG*1E6,'*-');
-    title("\epsilon"); ylabel("[\mum]"); xlabel("\sigma_{\Deltap/p} []");
+    plot(xVals,emiG*1E6,'*-');
+    title("\epsilon"); ylabel("[\mum]"); xlabel(myXlabel);
     grid on;
+    if ( exist('labels','var') && size(beta,2)==1 ), xticks(1:length(labels)); xticklabels(labels); xtickangle(45); end
+    if ( size(beta,2)>1 ), legend(labels,'Location','best'); end
 
     %% second row of plots: d and dp
     ax4=subplot(2,3,4);
-    plot(sigdpp,disp,'*-');
-    title("D"); ylabel("[m]"); xlabel("\sigma_{\Deltap/p} []");
+    plot(xVals,disp,'*-');
+    title("D"); ylabel("[m]"); xlabel(myXlabel);
     grid on;
+    if ( exist('labels','var') && size(beta,2)==1 ), xticks(1:length(labels)); xticklabels(labels); xtickangle(45); end
+    if ( size(beta,2)>1 ), legend(labels,'Location','best'); end
     %
     ax5=subplot(2,3,5);
-    plot(sigdpp,dispP,'*-');
-    title("D'"); ylabel("[]"); xlabel("\sigma_{\Deltap/p} []");
+    plot(xVals,dispP,'*-');
+    title("D'"); ylabel("[]"); xlabel(myXlabel);
     grid on;
-    
+    if ( exist('labels','var') && size(beta,2)==1 ), xticks(1:length(labels)); xticklabels(labels); xtickangle(45); end
+    if ( size(beta,2)>1 ), legend(labels,'Location','best'); end
+    %
+    if ( exist('labels','var') && size(beta,2)==1 )
+        ax6=subplot(2,3,6);
+        plot(xVals,sigdpp,'*-');
+        title("\sigma_{\Deltap/p}"); ylabel("[]"); xlabel(myXlabel);
+        grid on;
+        xticks(1:length(labels)); xticklabels(labels); xtickangle(45); 
+    end
+
     %% general
-    linkaxes([ax1 ax2 ax3 ax4 ax5],"x");
+    if ( exist('labels','var') )
+        linkaxes([ax1 ax2 ax3 ax4 ax5 ax6],"x");
+    else
+        linkaxes([ax1 ax2 ax3 ax4 ax5],"x");
+    end
     sgtitle(sprintf("%s plane",plane));
 end
--- optics/ShowFittedOrbits.m
+++ optics/ShowFittedOrbits.m
@@ -1,4 +1,4 @@
-function ShowFittedOrbits(z,zp,D,Dp,plane,avedpp)
+function ShowFittedOrbits(z,zp,D,Dp,plane,avedpp,labels)
 % showFittedOrbits              show the results of the reconstructed
 %                                  orbits based on the fit vs D/Dp
 %                                  (scan version)
@@ -6,26 +6,55 @@ function ShowFittedOrbits(z,zp,D,Dp,plane,avedpp)
 % input:
 % - z [m], zp []: orbits as reconstructed from fits;
 % - D [m], Dp []: dispersion and dispersion prime used to perform the scans;
-% - avedpp []: average delta_p_over_p;
 % - plane: current plane being plotted;
-% Nota Bene: z, zp are arrays as long as D and Dp;
+% - avedpp []: average delta_p_over_p;
+% - labels (optional): string of labels, identifying different fit cases;
+%   if not given, the passed optics functions are assumed to be reconstructed
+%                 by a fit parameteric in avedpp; hence, z and zp are
+%                 arrays as long as D and Dp;
 %
 %
 % See also ShowFittedOpticsFunctions.
 
     figure();
+    if ( size(z,2)>1 )
+        % more data sets
+        legends=strings(size(z,2),1);
+        if ( exist('labels','var') )
+            for ii=1:size(z,2), legends(ii)=sprintf("%s - \\delta_{ave}=%g",labels(ii),avedpp(ii)); end
+        else
+            for ii=1:size(z,2), legends(ii)=sprintf("\\delta_{ave}=%g",avedpp(ii)); end
+        end
+    end
     %
     ax1=subplot(1,2,1);
-    plot(D,z,'*-');
-    title("z_0"); ylabel("[m]"); xlabel("D [m]");
+    if ( exist('labels','var') && size(z,2)==1 )
+        plot(1:length(labels),z,'*-');
+        xticks(1:length(labels)); xticklabels(labels); xtickangle(45); 
+    else
+        plot(D,z,'*-');
+        xlabel("D [m]");
+    end
+    title("z_0"); ylabel("[m]");
     grid on;
+    if ( size(z,2)>1 ), legend(legends,'Location','best'); end
     %
     ax2=subplot(1,2,2);
-    plot(Dp,zp,'*-');
-    title("z'_0"); ylabel("[]"); xlabel("D' []");
+    if ( exist('labels','var') && size(z,2)==1 )
+        plot(1:length(labels),z,'*-');
+        xticks(1:length(labels)); xticklabels(labels); xtickangle(45); 
+    else
+        plot(Dp,zp,'*-');
+        xlabel("D' []");
+    end
+    title("z'_0"); ylabel("[]");
     grid on;
     %
     % linkaxes([ax1 ax2],"x");
-    sgtitle(sprintf("%s plane - \delta_{ave}=%g",plane,avedpp));
-    
+    if ( size(z,2)>1 ), legend(legends,'Location','best'); end
+    if ( size(z,2)==1 )
+        sgtitle(sprintf("%s plane - \\delta_{ave}=%g",plane,avedpp)); % only one set of data
+    else
+        sgtitle(sprintf("%s plane",plane)); % only one set of data
+    end    
 end
