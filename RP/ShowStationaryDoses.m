bfdd0246ea0b4a8307c2d37d0270c5512162666c
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-09-27 10:58:05 +0200

    ShowStationaryDoses has three optional input vars: means, maxs, mins. Getting ready to use it also for polymaster

--- RP/ShowStationaryDoses.m
+++ RP/ShowStationaryDoses.m
@@ -1,4 +1,4 @@
-function ShowStationaryDoses(tStamps,doses,means,maxs,mins)
+function ShowDoses(tStamps,doses,means,maxs,mins)
     fprintf("plotting doses...\n");
     nMonitors=size(tStamps,2);
     [nRows,nCols]=GetNrowsNcols(nMonitors);
@@ -9,12 +9,22 @@ function ShowStationaryDoses(tStamps,doses,means,maxs,mins)
         % raw data
         subplot(nRows,nCols,iMonitor);
         Xs=datenum(tStamps(:,iMonitor));
-        plot(Xs,doses(:,iMonitor),".",...
-             Xs,means(:,iMonitor),"b-",...
-             Xs,mins(:,iMonitor),"r-",...
-             Xs,maxs(:,iMonitor),"r-" );
+        tmpLeg="data";
+        plot(Xs,doses(:,iMonitor),".");
+        if ( exist('means','var') )
+            hold on; plot(Xs,means(:,iMonitor),"b-");
+            tmpLeg=[tmpLeg "mean"];
+        end
+        if ( exist('mins','var') )
+            hold on; plot(Xs,mins(:,iMonitor),"r-");
+            tmpLeg=[tmpLeg "min"];
+        end
+        if ( exist('maxs','var') )
+            hold on; plot(Xs,maxs(:,iMonitor),"r-");
+            tmpLeg=[tmpLeg "max"];
+        end
         xlabel("time"); grid(); ylabel("dose [\muSv]"); title("raw data");
-        legend("data","mean","min","max");
+        legend(tmpLeg);
         datetick('x','dd-mm HH:MM:ss');
         
         % get ready for new data set
