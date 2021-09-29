7990d04cf5f46898481b9424b6c83826189380a2
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-09-29 13:00:25 +0200

    parsing of cameretta summary files and DCT log data can be performed on more than a single path

--- measurement_analysis/ParseCAMSummaryFiles.m
+++ measurement_analysis/ParseCAMSummaryFiles.m
@@ -1,8 +1,8 @@
-function [cyProgs,cyCodes,BARs,FWHMs,ASYMs,INTs]=ParseCAMSummaryFiles(path2Files)
+function [cyProgs,cyCodes,BARs,FWHMs,ASYMs,INTs]=ParseCAMSummaryFiles(paths2Files)
 % ParseCAMSummaryFiles        parse summary files of Cameretta (ministrip monitor)
 %
 % input:
-% - path2Files: path where the file(s) is located (a dir command is anyway performed).
+% - paths2Files (array of strings): path(s) where the file(s) is located (a dir command is anyway performed).
 % output:
 % - cyProgs (array of strings): cycle prog associated to each event;
 % - cyCodes (array of strings): cycle code associated to each event;
@@ -18,36 +18,39 @@ function [cyProgs,cyCodes,BARs,FWHMs,ASYMs,INTs]=ParseCAMSummaryFiles(path2Files
 % - 1 header line;
 % - a line for each cycle prog; the format of the line is eg: "6000ECC0900	-9.297985	-10.947896	35.661529	34.325630	0.398755	6.695736	2.640946E+9	2.443980E+9	195453981"
 
-    files=dir(path2Files);
-    nDataSets=length(files);
-    fprintf("acquring %i data sets in %s ...\n",nDataSets,path2Files);
+    fprintf("acquring summary data from Cameretta...\n");
     nReadFiles=0;
     nCountsTot=0;
-    for iSet=1:nDataSets
-        fileName=strcat(files(iSet).folder,"\",files(iSet).name);
-        fileID = fopen(fileName,"r");
-        C = textscan(fileID,'%s %f %f %f %f %f %f %f %f %s','HeaderLines',1);
-        fclose(fileID);
-        nCounts=length(C{:,1});
-        cyCodes(nCountsTot+1:nCountsTot+nCounts)=C{:,1};
-        for ii=1:2
-            BARs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,1+ii};
-            FWHMs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,3+ii};
-            ASYMs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,5+ii};
-            INTs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,7+ii};
+    for iPath=1:length(paths2Files)
+        files=dir(paths2Files(iPath));
+        nDataSets=length(files);
+        fprintf("...acquring %i data sets in %s ...\n",nDataSets,paths2Files(iPath));
+        for iSet=1:nDataSets
+            fileName=strcat(files(iSet).folder,"\",files(iSet).name);
+            fileID = fopen(fileName,"r");
+            C = textscan(fileID,'%s %f %f %f %f %f %f %f %f %s','HeaderLines',1);
+            fclose(fileID);
+            nCounts=length(C{:,1});
+            cyCodes(nCountsTot+1:nCountsTot+nCounts)=C{:,1};
+            for ii=1:2
+                BARs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,1+ii};
+                FWHMs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,3+ii};
+                ASYMs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,5+ii};
+                INTs(nCountsTot+1:nCountsTot+nCounts,ii)=C{:,7+ii};
+            end
+            cyProgs(nCountsTot+1:nCountsTot+nCounts)=C{:,10};
+            fprintf("   ...acquired %d entries in file %s...\n",nCounts,files(iSet).name);
+            nCountsTot=nCountsTot+nCounts;
+            nReadFiles=nReadFiles+1;
         end
-        cyProgs(nCountsTot+1:nCountsTot+nCounts)=C{:,10};
-        nCountsTot=nCountsTot+nCounts;
-        fprintf("...acquired %d entries in file %s...\n",nCounts,files(iSet).name);
-        nReadFiles=nReadFiles+1;
     end
-    if ( nDataSets>0 )
+    if ( nReadFiles>0 )
         % try to return a vector array, not a row
         if ( size(cyProgs,2)>size(cyProgs,1) )
             cyProgs=cyProgs';
             cyCodes=cyCodes';
         end
-        if ( nDataSets>1 )
+        if ( nReadFiles>1 )
             [cyProgs,cyCodes,ids]=SortByTime(cyProgs,cyCodes); % sort by cycle progs
             BARs(:,:)=BARs(ids,:);
             FWHMs(:,:)=FWHMs(ids,:);
@@ -63,5 +66,4 @@ function [cyProgs,cyCodes,BARs,FWHMs,ASYMs,INTs]=ParseCAMSummaryFiles(path2Files
         INTs=missing;
     end
     fprintf("...acqured %i files, for a total of %d entries;\n",nReadFiles,nCountsTot);
-    
 end
