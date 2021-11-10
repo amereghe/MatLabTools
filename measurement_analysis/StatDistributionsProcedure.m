7d74889211ffb446e24af70497ecc4b8b144a40f
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-11-10 14:29:24 +0100

    fixing calculation of integrals when computing statistics figures with BD procedure

--- measurement_analysis/StatDistributionsProcedure.m
+++ measurement_analysis/StatDistributionsProcedure.m
@@ -7,12 +7,13 @@ function [BARs,FWHMs,INTs]=StatDistributionsProcedure(data)
     for iSet=1:nDataSets
         tmpXs(:,1:2)=data(:,1,:);      % (nFibers,2)
         tmpYs(:,1:2)=data(:,1+iSet,:); % (nFibers,2)
+        INTs(iSet,1:2)=sum(tmpYs);
         tmpYs(tmpYs<200)=0.0;
         for ii=1:2
-            INTs(iSet,ii)=sum(tmpYs(:,ii));
-            if ( INTs(iSet,ii)>0 )
-                BARs(iSet,ii)=sum(tmpXs(:,ii).*tmpYs(:,ii))/INTs(iSet,ii);
-                FWHMs(iSet,ii)=sqrt(sum(((tmpXs(:,ii)-BARs(iSet,ii)).^2).*tmpYs(:,ii))/INTs(iSet,ii));
+            norm=sum(tmpYs(:,ii));
+            if ( norm>0 )
+                BARs(iSet,ii)=sum(tmpXs(:,ii).*tmpYs(:,ii))/norm;
+                FWHMs(iSet,ii)=sqrt(sum(((tmpXs(:,ii)-BARs(iSet,ii)).^2).*tmpYs(:,ii))/norm);
             end
         end
     end
