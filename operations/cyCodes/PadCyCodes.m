36e74c9abd26abb7aaf15f248613b2189b280e1d
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-02-07 09:19:40 +0100

    forgot function for previous commit

--- operations/cyCodes/PadCyCodes.m
+++ operations/cyCodes/PadCyCodes.m
@@ -1,4 +1,7 @@
 function cyCodesOut=PadCyCodes(cyCodesIN)
 % PadCyCodes       head cyCodes with 0, ie cyCodes<12 chars: head as many "0" as needed
-    cyCodesOut=pad(cyCodesIN,12,"left","0");
+    cyCodesOut=cyCodesIN;
+    % pay attention to missing cyCodes
+    indices=(~ismissing(cyCodesOut));
+    cyCodesOut(indices)=pad(cyCodesOut(indices),12,"left","0");
 end
