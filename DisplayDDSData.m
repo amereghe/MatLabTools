d596e9ac6d5273f9785bceee80a72fd878b51425
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-04-13 12:46:44 +0200

    adding comparison between summary data and statistics on profiles

--- DisplayDDSData.m
+++ DisplayDDSData.m
@@ -9,33 +9,40 @@ pathToLibrary=".\";
 addpath(genpath(pathToLibrary));
 
 %% settings
-clear kPath DDSpathMain DDSpaths
+clear kPath DDSpathMain DDSpaths DDSsummFiles DDSprofFiles
 
 kPath="S:\Accelerating-System\Accelerator-data";
 % kPath="K:";
 DDSpathMain="T:\AMereghetti\DDS_for_RP";
 
-DDSpaths=[...
+DDSpaths=[
     strcat(DDSpathMain,"\PRC-544-220410*") 
     ];
 
+DDSsummFiles=DDSpaths+"\Data-*.csv";
+DDSprofFiles=DDSpaths+"\Profiles\Data-*.csv";
+
 %% parse files
 % - clear summary data
-clear DScyProgsSumm DDScyCodesSumm DDSbarsSumm DDSfwhmsSumm DDSasymsSumm DDSintsSumm
+clear DDScyProgsSumm DDScyCodesSumm DDSbarsSumm DDSfwhmsSumm DDSasymsSumm DDSintsSumm
 % - clear profiles
 clear DDSprofiles DDScyCodes DDScyProgs
 
 % - parse DDS summary files
-[DScyProgsSumm,DDScyCodesSumm,DDSbarsSumm,DDSfwhmsSumm,DDSasymsSumm,DDSintsSumm]=ParseCAMSummaryFiles(DDSpaths+"\Data-*.csv","DDS",0);
-if (length(DScyProgsSumm)<=1), error("...no summary data aquired!"); end
+[DDScyProgsSumm,DDScyCodesSumm,DDSbarsSumm,DDSfwhmsSumm,DDSasymsSumm,DDSintsSumm]=ParseCAMSummaryFiles(DDSsummFiles,"DDS",0);
+if (length(DDScyProgsSumm)<=1), error("...no summary data aquired!"); end
 % - parse DDS profiles
-[DDSprofiles,DDScyCodes,DDScyProgs]=ParseDDSProfiles(DDSpaths+"\Profiles\Data-*.csv");
+[DDSprofiles,DDScyCodes,DDScyProgs]=ParseDDSProfiles(DDSprofFiles,"DDS");
 if (length(DDScyProgs)<=1), error("...no summary data aquired!"); end
+% - get statistics out of profiles
+[BARs,FWHMs,INTs]=StatDistributionsBDProcedure(DDSprofiles);
 
-if ( length(DScyProgsSumm)~=length(DDScyProgs) ), error("...inconsistent data set between summary data and actual profiles"); end
+if ( length(DDScyProgsSumm)~=length(DDScyProgs) ), error("...inconsistent data set between summary data and actual profiles"); end
 
 %% show data
 % - 3D plot
 ShowSpectra(DDSprofiles,sprintf("DDS profiles in %s",DDSpathMain));
 % - series
 ShowDDSSummaryData(DDSbarsSumm,DDSfwhmsSumm,DDSasymsSumm,DDSintsSumm);
+% - compare summary data and statistics on profiles
+CompareDDSSummaryAndProfiles(DDSbarsSumm,DDSfwhmsSumm,DDSintsSumm,BARs,FWHMs,INTs);
