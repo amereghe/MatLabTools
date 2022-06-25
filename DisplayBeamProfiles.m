83cee43e39e44a9cce8263dc77d51d655fdb2c64
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-06-25 04:45:26 +0200

    implementing possibility to crunch SFH/SFM profiles in script showing beam profiles

--- DisplayBeamProfiles.m
+++ DisplayBeamProfiles.m
@@ -1,14 +1,17 @@
 % {}~
 
 %% description
-% this is a script which parses DDS/CAMeretta files and plots them
-% - the script crunches as many DDS/CAMeretta files as desired, provided the
-%   fullpaths;
-% - both summary files and actual profiles in the same path are aquired;
+% this is a script which parses beam profiles files and plots them
+% - the script crunches as many files as desired, provided the fullpaths;
+% - for the time being, only CAMeretta/DDS/SFH/SFM monitors;
+% - for CAMeretta/DDS: both summary files and actual profiles in the same
+%   path are aquired;
+% - for SFH/SFM: profiles are acquired, but only the integral ones are
+%   shown;
 % - the script visualises in 3D the spill-per-spill profiles, horizontal and
 %   vertical planes separately;
-% - the script shows summary data, and compares summary data against
-%   statistics data computed on profiles;
+% - the script shows summary data; for CAMeretta/DDS only, the script also
+%   compares summary data against statistics data computed on profiles;
 
 %% include libraries
 % - include Matlab libraries
@@ -20,28 +23,29 @@ clear kPath MonPathMain MonPaths SummFiles ProfFiles
 
 % -------------------------------------------------------------------------
 % USER's input data
-kPath="S:\Area Ricerca\EMITTANZE SUMMARY\EMITTANZE SUMMARY";
+kPath="D:\GuessOptics\data\PROTON\090mm\HEBT";
 % kPath="K:";
-MonPathMain="\2022\Emittanze 2022\secondo giro\Scan U1-08-H23";
+MonPathMain="\alone_H5-009A-QUE";
 MonPaths=[...
-    strcat(kPath,MonPathMain,"\CarbSO1_LineU_Size6_13-03-2022_2002") 
-%     strcat(kPath,MonPathMain,"\PRC-544-220313-2001") 
+    strcat(kPath,MonPathMain,"\PRC-544-220109-0259_U1-021B-SFM") 
     ];
-monType="CAM"; % DDS, CAM
-myFigTitle=monType;
+monType="SFM"; % DDS, CAM, SFH/SFM
+myTit=sprintf("%s profiles in %s",monType,MonPathMain);
 % -------------------------------------------------------------------------
 
 %% parse files
 % - files to crunch:
 switch upper(monType)
-    case "DDS"
-        SummFiles=MonPaths+"\Data-*.csv";
-        ProfFiles=MonPaths+"\Profiles\Data-*.csv";
     case "CAM"
         SummFiles=MonPaths+"\*summary.txt";
         ProfFiles=MonPaths+"\profiles\*_profiles.txt";
+    case "DDS"
+        SummFiles=MonPaths+"\Data-*.csv";
+        ProfFiles=MonPaths+"\Profiles\Data-*.csv";
+    case {"SFH","SFM"}
+        ProfFiles=MonPaths+"\Data-*.csv";
     otherwise
-        error("Mon type NOT recognised: %s! - presently only DDS and CAM are available",monType);
+        error("Mon type NOT recognised: %s! - presently only CAM/DDS/SFM/SFH are available",monType);
 end
 
 % - clear summary data
@@ -49,31 +53,46 @@ clear cyProgsSumm cyCodesSumm BarsSumm FwhmsSumm AsymsSumm IntsSumm
 % - clear profiles
 clear profiles cyCodes cyProgs
 
-% - parse summary files
-[cyProgsSumm,cyCodesSumm,BarsSumm,FwhmsSumm,AsymsSumm,IntsSumm]=ParseCAMSummaryFiles(SummFiles,monType,0);
-if (length(cyProgsSumm)<=1), error("...no summary data aquired!"); end
-
-% - parse profiles
-[profiles,cyCodes,cyProgs]=ParseDDSProfiles(ProfFiles,monType);
-if (length(cyProgs)<=1), error("...no profiles aquired!"); end
-% - get statistics out of profiles
+% - parse files
 switch upper(monType)
-    case "CAM"
-        [BARs,FWHMs,INTs]=StatDistributionsCAMProcedure(profiles);
-    otherwise
-        [BARs,FWHMs,INTs]=StatDistributionsBDProcedure(profiles);
+    case {"CAM","DDS"}
+        % - parse summary files
+        [cyProgsSumm,cyCodesSumm,BarsSumm,FwhmsSumm,AsymsSumm,IntsSumm]=ParseCAMSummaryFiles(SummFiles,monType,0);
+        if (length(cyProgsSumm)<=1), error("...no summary data aquired!"); end
+        % - parse profiles
+        [profiles,cyCodes,cyProgs]=ParseDDSProfiles(ProfFiles,monType);
+        if (length(cyProgs)<=1), error("...no profiles aquired!"); end
+        % - quick check of consistency of parsed data
+        if ( length(cyProgsSumm)~=length(cyProgs) ), error("...inconsistent data set between summary data and actual profiles"); end
+    
+    otherwise % SFH,SFM
+        % - parse profiles
+        [diffProfiles,cyCodes,cyProgs]=ParseSFMData(ProfFiles,monType);
+        if (length(cyProgs)<=1), error("...no profiles aquired!"); end
+        % - get integral profiles
+        profiles=SumSpectra(diffProfiles); 
 end
 
-% - quick check of consistency of parsed data
-if ( length(cyProgsSumm)~=length(cyProgs) ), error("...inconsistent data set between summary data and actual profiles"); end
+% - get statistics out of profiles
+if ( strcmpi(monType,"CAM") )
+    [BARs,FWHMs,INTs]=StatDistributionsCAMProcedure(profiles);
+else
+    [BARs,FWHMs,INTs]=StatDistributionsBDProcedure(profiles);
+end
 
 %% show data
 % - 3D plot
-ShowSpectra(profiles,sprintf("%s profiles in %s",monType,MonPathMain));
-% - series
-ShowBeamProfilesSummaryData(BarsSumm,FwhmsSumm,AsymsSumm,IntsSumm,myFigTitle);
-% - compare summary data and statistics on profiles
-CompBars=BarsSumm; CompBars(:,:,2)=BARs;
-CompFwhms=FwhmsSumm; CompFwhms(:,:,2)=FWHMs;
-CompInts=IntsSumm; CompInts(:,:,2)=INTs;
-CompareStats(CompBars,CompFwhms,CompInts,missing(),["stat on profiles" "summary data"]);
+ShowSpectra(profiles,sprintf("%s - 3D profiles",myTit));
+% - other plots
+if ( strcmpi(monType,"CAM") || strcmpi(monType,"DDS") )
+    % - series
+    ShowBeamProfilesSummaryData(BarsSumm,FwhmsSumm,IntsSumm,AsymsSumm,missing(),"summary data",missing(),myTit);
+    % - compare summary data and statistics on profiles
+    CompBars=BarsSumm; CompBars(:,:,2)=BARs;
+    CompFwhms=FwhmsSumm; CompFwhms(:,:,2)=FWHMs;
+    CompInts=IntsSumm; CompInts(:,:,2)=INTs;
+    ShowBeamProfilesSummaryData(CompBars,CompFwhms,CompInts,missing(),missing(),["stat on profiles" "summary data"],missing(),sprintf("%s - summary vs profile stats",myTit));
+else
+    % - series
+    ShowBeamProfilesSummaryData(BARs,FWHMs,INTs,missing(),missing(),"integral profiles",missing(),myTit);
+end
--- measurement_analysis/ShowBeamProfilesSummaryData.m
+++ measurement_analysis/ShowBeamProfilesSummaryData.m
@@ -1,30 +1,84 @@
-function ShowBeamProfilesSummaryData(bars,fwhms,asyms,ints,myFigTitle)
-    figure();
-    planes=["HOR" "VER"];
+function ShowBeamProfilesSummaryData(BARs,SIGs,INTs,ASYMs,indices,labels,whatNames,myTitle)
+% ShowBeamProfilesSummaryData      to create a figure where Baricentres, SIGmas/FWHMs,
+%                                   INTegrals and ASYMmetries of beam
+%                                   profiles are shown;
+% 
+% in case of multiple data sets, they are compared.
+% 
+% the function creates a stand-alone figure with 2x3(4) plots (hor/ver x
+% SIG/BAR/INT(/ASYM)), comparing the various quantities;
+% 
+% input:
+% - BARs (float(nScanPoints,nPlanes,nSets)): baricentres of distributions [mm];
+% - SIGs (float(nScanPoints,nPlanes,nSets)): sigmas of distributions [mm];
+% - INTs (float(nScanPoints,nPlanes,nSets)): integrals of distributions [mm];
+% - ASYMs (float(nScanPoints,nPlanes,nSets), can be missing): asymmetry [mm];
+% - indices ([IDmin IDmax], optional): IDs of the scan points to be taken;
+% - labels (strings(nSets)): a label for each scan;
+% - whatNames (strings(nCols)): a label for each quantity;
+% - myTitle (string): figure title;
+% 
+
+    if ( ~exist('indices','var') ), indices=missing(); end
+    if ( ~exist('labels','var') ), labels=missing(); end
+    if ( ~exist('whatNames','var') ), whatNames=missing(); end
     
-    for iWhat=1:4
-        switch iWhat
-            case 1
-                what=bars; myYlab="%s [mm]"; myTitle="BARicentre";
-            case 2
-                what=fwhms; myYlab="%s [mm]"; myTitle="FWHM";
-            case 3
-                what=ints; myYlab="%s [counts]"; myTitle="INTegrals";
-            case 4
-                what=asyms; myYlab="%s [mm]"; myTitle="ASYmmetry";
-        end
-        axs(iWhat)=subplot(2,2,iWhat);
-        for iPlane=1:length(planes)
-            if ( iPlane>1 ), hold on; end
-            plot(1:size(what,1),what(:,iPlane),".-");
-        end
-        grid(); title(myTitle);
-        xlabel("ID []"); ylabel(sprintf(myYlab,myTitle));
-        legend(planes+" plane","Location","best");
+    nCols=3; % SIGs, BARs, INTs
+    if ( ~ismissing(ASYMs) ), nCols=4; end
+    planes=["Hor" "Ver"];
+    nRows=length(planes); % Hor, Ver
+    nSets=size(INTs,3);
+
+    if ( ismissing(indices) )
+        iis=1:size(INTs,1)';
+    else
+        iis=(indices(1):indices(2))';
+    end
+    if ( ismissing(labels) )
+        labels=compose("Series %02i",(1:nSets)');
+    end
+    if ( ismissing(whatNames) )
+        whatNames=["FWHM" "BAR" "INT" "ASYM"];
     end
     
-    % global
-    linkaxes(axs,"x");
-    if (exist("myFigTitle","var")), sgtitle(myFigTitle); end
+    % actually generate figure
+    if (exist("myTitle","var"))
+        figure("Name",LabelMe(myTitle));
+    else
+        figure();
+    end
+    iPlot=0;
+    if ( nSets==2 )
+        markers=[ "o" "*" ];
+    else
+        markers=strings(nSets,1);
+        markers="*";
+    end
+    for iRow=1:nRows
+        for iCol=1:nCols
+            iPlot=iPlot+1;
+            subplot(nRows,nCols,iPlot);
+            switch iCol
+                case 1
+                    what=SIGs; myYLab="[mm]";
+                case 2
+                    what=BARs; myYLab="[mm]";
+                case 3
+                    what=INTs; myYLab="[]";
+                case 4
+                    what=ASYMs; myYLab="[mm]";
+            end
+            for iSet=1:nSets
+                if ( iSet>1 ), hold on; end
+                plot(iis,what(iis,iRow,iSet),"-","Marker",markers(iSet));
+            end
+            xlabel("ID []"); ylabel(myYLab); grid on; title(sprintf("%s - %s",planes(iRow),whatNames(iCol)));
+            legend(labels,"location","best");
+        end
+    end
+    
+    if (exist("myTitle","var"))
+        sgtitle(LabelMe(myTitle));
+    end
     
 end
