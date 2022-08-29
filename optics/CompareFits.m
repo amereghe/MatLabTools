0892e65f23a889a5b51d874343971c2a6c1dc51b
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-08-29 17:13:47 +0200

    rationalising CompareFits.m

--- optics/CompareFits.m
+++ optics/CompareFits.m
@@ -1,88 +1,79 @@
-function CompareFits(calcY,xVals,measY,measCurr,what,par1Names,par2Names,planeNames,myXlabel,par3Names,myTit,myMon,labelsType)
+function CompareFits(calcY,xVals,measY,measCurr,what,myXlabel,myYlabel,colNames,rowNames,seriesNames,myTit,myMon,nSeriesPerPlane,outName)
     if ( ~strcmpi(what,"SIG") && ~strcmpi(what,"BAR") )
         error("you can compare either SIGmas or BARicentres!");
     end
     
     nXs=size(calcY,1);
-    nPar1=size(calcY,2); % eg: sigdpp
-    nPar2=size(calcY,3); % eg: fracEst
-    nPlanes=size(calcY,4);
-    nPar3=size(calcY,5); % eg: fit set
+    nRows=size(calcY,2); % eg: planes
+    nCols=size(calcY,4); % eg: fracEst
     
     %% checks
     if ( size(measY,1)~=nXs )
         error("Measured data must have as many values as the calculated ones!");
     end
-    if ( size(measY,2)~=nPar1 )
-        error("Measured data must be parametrised as the calculated ones (2nd dim)!");
-    end
-    if ( size(measY,3)~=nPar2 )
-        error("Measured data must be parametrised as the calculated ones (3rd dim)");
+    if ( size(measY,2)~=nRows )
+        error("Measured data must be parametrised as the calculated ones (2nd dim)");
     end   
+    if ( size(measY,3)~=nCols )
+        error("Measured data must be parametrised as the calculated ones (3rd dim)!");
+    end
     
     %% actual plotting
-    for iType=1:length(labelsType)
-        for iPar1=1:nPar1 % eg: sigdpp
-            figure();
-            cm=colormap(parula(nPar3));
-            ii=0;
-            for iPlane=1:nPlanes
-                for iPar2=1:nPar2 % eg: fractEst
-                    ii=ii+1; axs(iPar2,iPlane)=subplot(nPlanes,nPar2+1,ii);
-                    % - measurements
-                    if ( strcmpi(what,"SIG") )
-                        plot(measCurr(:,iPlane),measY(:,iPlane,iPar2,iType),"k*"); hold on;
-                    else
-                        plot(measCurr(:,iPlane),measY(:,iPlane,iPar2),"k*"); hold on;
-                    end
-                    % - fits
-                    for iPar3=1:nPar3 % eg: fit set
-                        hold on; plot(xVals(:,iPlane,iPar3),calcY(:,iPar1,iPar2,iPlane,iPar3,iType)*1E3,".-","color",cm(iPar3,:));
-                    end
-                    % - bin width
-                    if ( strcmpi(what,"SIG") )
-                        PlotMonsBinWidth(measCurr(:,iPlane),myMon);
-                    end
-                    % - general
-                    grid(); xlabel(myXlabel);
-                    if ( strcmpi(what,"SIG") )
-                        if ( strcmpi(labelsType(iType),"EMI_RMS") )
-                            ylabel("\sigma [mm]");
-                        elseif ( strcmpi(labelsType(iType),"EMI_HWxM") )
-                            ylabel("HWxM [mm]");
-                        end
-                        title(sprintf("%s plane - %s",planeNames(iPlane),par2Names(iPar2)));
-                    else
-                        ylabel("BAR [mm]");
-                        title(sprintf("%s plane",planeNames(iPlane)));
-                    end
-                end
-                % dedicated plot for the legend
-                if ( iPlane==1 )
-                    ii=ii+1; subplot(nPlanes,nPar2+1,ii);
-                    plot(NaN(),NaN(),"k*");
-                    for iPar3=1:nPar3
-                        hold on; plot(NaN(),NaN(),".-","Color",cm(iPar3,:));
-                    end
-                    if ( strcmpi(what,"SIG") )
-                        hold on; plot(NaN(),NaN(),"r-");
-                        legends=strings(nPar3+2,1);
-                        legends(1)="measurements";
-                        legends(2:end-1)=par3Names;
-                        legends(end)="MON bin width";
-                    else
-                        legends=strings(nPar3+1,1);
-                        legends(1)="measurements";
-                        legends(2:end)=par3Names;
-                    end
-                    legend(legends,"Location","best");
-                end
+    ff=figure();
+    cm=colormap(parula(max(nSeriesPerPlane)));
+    ff.Position(1:2)=[0 0]; % figure at lower-left corner of screen
+    ff.Position(3)=ff.Position(3)*nCols*(2./3.); % larger figure:
+    ff.Position(4)=ff.Position(4)*nRows*(2./3.); % larger figure:
+    t = tiledlayout(nRows,nCols+1,'TileSpacing','Compact','Padding','Compact'); % minimise whitespace around plots
+    ii=0;
+    for iRow=1:nRows % eg: plane
+        for iCol=1:nCols % eg: fractEst
+            ii=ii+1; axs(iRow,iCol)=nexttile; % subplot(nRows,nCols+1,ii);
+            % - measurements
+            plot(measCurr(:,iRow),measY(:,iRow,iCol),"k*"); hold on;
+            % - fits
+            for iSeries=1:nSeriesPerPlane(iRow)
+                hold on; plot(xVals(:,iRow,iSeries),calcY(:,iRow,iSeries,iCol)*1E3,".-","color",cm(iSeries,:));
             end
-            if ( nPar2>1 )
-                linkaxes(axs(:,1),"xy"); % all HOR quantities
-                linkaxes(axs(:,2),"xy"); % all VER quantities
+            % - bin width
+            if ( strcmpi(what,"SIG") )
+                PlotMonsBinWidth(measCurr(:,iRow),myMon);
             end
-            sgtitle(sprintf("%s - %s - %s - %s",myTit,myMon,par1Names(iPar1),LabelMe(labelsType(iType))));
+            % - general
+            grid(); xlabel(myXlabel); ylabel(myYlabel);
+            title(sprintf("%s plane - %s",rowNames(iRow),colNames(iCol)));
+        end
+        % dedicated plot for the legend
+        if ( iRow==1 )
+            ii=ii+1; nexttile; % subplot(nRows,nCols+1,ii);
+            plot(NaN(),NaN(),"k*");
+            for iSeries=1:nSeriesPerPlane(iRow)
+                hold on; plot(NaN(),NaN(),".-","Color",cm(iSeries,:));
+            end
+            if ( strcmpi(what,"SIG") )
+                hold on; plot(NaN(),NaN(),"r-");
+                legends=strings(nSeriesPerPlane(iRow)+2,1);
+                legends(1)="measurements";
+                legends(2:end-1)=seriesNames(1:nSeriesPerPlane(iRow));
+                legends(end)="MON bin width";
+            else
+                legends=strings(nSeriesPerPlane(iRow)+1,1);
+                legends(1)="measurements";
+                legends(2:end)=seriesNames(1:nSeriesPerPlane(iRow));
+            end
+            legend(legends,"Location","best");
+        end
+    end
+    if ( nCols>1 )
+        linkaxes(axs(1,:),"xy"); % all HOR quantities
+        linkaxes(axs(2,:),"xy"); % all VER quantities
+    end
+    sgtitle(myTit);
+    % save figure
+    if ( exist('outName','var') )
+        if ( strlength(outName)>0 )
+            savefig(outName);
+            fprintf("...saving to file %s ...\n",outName);
         end
     end
 end
