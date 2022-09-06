4d0dea38cdb392d3dd414f1d1b03849d8759fc06
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-09-06 10:17:06 +0200

    adding function which stores bin width of monitors

--- measurement_analysis/PlotMonsBinWidth.m
+++ measurement_analysis/PlotMonsBinWidth.m
@@ -1,11 +1,5 @@
 function PlotMonsBinWidth(xVals,myMon,color)
     if ( ~exist("color","var") ), color="r"; end
-    switch upper(extractBetween(myMon,1,3))
-        case {"GIM"}
-            hold on; plot([min(xVals) max(xVals)],[0.5 0.5],"-","Color",color);
-        case {"CAM","SFM","SFH","SFP"}
-            hold on; plot([min(xVals) max(xVals)],[1 1],"-","Color",color);
-        case "DDS"
-            hold on; plot([min(xVals) max(xVals)],[1.65 1.65],"-","Color",color);
-    end
+    BinWidth=ReturnMonBinWidth(myMon);
+    hold on; plot([min(xVals) max(xVals)],[BinWidth BinWidth],"-","Color",color);
 end
