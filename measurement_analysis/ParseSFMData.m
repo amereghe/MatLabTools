e1b8334372c98bea9ad6eb132a9115b21afc0cf1
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-10-14 10:25:57 +0200

    when parsing profiles, a loop over more than a single folder can be done

--- measurement_analysis/ParseSFMData.m
+++ measurement_analysis/ParseSFMData.m
@@ -1,11 +1,11 @@
-function [measData,cyCodes,cyProgs]=ParseSFMData(path2Files,fFormat)
+function [measData,cyCodes,cyProgs]=ParseSFMData(paths2Files,fFormat)
 % ParseSFMData     parses distributions recorded by CAM, DDS, GIM, PIB/PMM,
 %                     QBM and SFH/SFM/SFP;
 %                  for the time being, the function does not parse QIM data
 %
 % input:
-% - path2Files [string]: path were files to be parsed are stored; it can be
-%   a regexp, e.g. "PRC-544-210509-1437-Z2-021B-SFM\Data-*-*-Z2-021B-SFM.csv"
+% - paths2Files (array of strings): path(s) where the file(s) is(are)
+%   located. A regexp can be given, e.g. "PRC-544-210509-1437-Z2-021B-SFM\Data-*-*-Z2-021B-SFM.csv"
 % - fFormat [string, optional]: detector that generated the data; this
 %   parameter sets formats for reading data; default: SFM
 %
@@ -13,6 +13,7 @@ function [measData,cyCodes,cyProgs]=ParseSFMData(path2Files,fFormat)
 % - measData [float(max(Nx,Ny),maxColumns,2,nDataSets)]: array of data;
 %   . Nx,Ny: number of hor/ver fibers;
 %   . maxColumns: number of time acquisitions + values of X (first column);
+%     in case of CAM/DDS, there is only an acquisition per plane;
 %   . 2: planes: 1: hor; 2: ver;
 %   . nDataSets: number of files;
 % - cyCodes [string(nDataSets,1)]: array of cycle codes;
@@ -57,152 +58,156 @@ function [measData,cyCodes,cyProgs]=ParseSFMData(path2Files,fFormat)
     cyProgs=NaN(actualDataSets,1);
     cyCodes=strings(actualDataSets,1);
     
-    %% filename and extension
-    [filepath,name,ext]=fileparts(path2Files);
-    if (strlength(name)==0)
-        switch upper(fFormat)
-            case "GIM"
-                name="XY_*";
-            case "SFP"
-                name="Data-*";
-            otherwise
-                error("...please specify name for %s profile files",fFormat);
-        end
-    end
-    if (strlength(ext)==0)
-        switch upper(fFormat)
-            case "GIM"
-                ext=".txt";
-            case "SFP"
-                ext=".csv";
-            otherwise
-                error("...please specify extension for %s profile files",fFormat);
-        end
-    end
-    
+
     %% actually parse
-    files=dir(strcat(filepath,"\",name,ext));
-    nDataSets=length(files);
-    fprintf("acquring %i data sets with %s format...\n",nDataSets,upper(fFormat));
-    
-    nAcq=0;
-    for iSet=1:nDataSets
-        switch upper(fFormat)
-            case "CAM"
-                nAcq=nAcq+1;
-                fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
-                % check cycle prog, to guarantee continuity
-                tmp=split(files(iSet).name,"_");
-                tmpCyProg=str2num(tmp{1});
-                tmpCyCode=string(tmp{2});
-                if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
-                    % fast forward with NaNs
-                    [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
-                end
-                %
-                tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',1,'MultipleDelimsAsOne',true));
-                % x-axis values
-                measData(1:Nx,1,1)=tmp(1:Nx,1);                % fiber positions
-                measData(1:Nx,1+actualDataSets,1)=tmp(1:Nx,2); % values
-                % y-axis values
-                measData(1:Ny,1,2)=tmp(1:Ny,1);                % fiber positions
-                measData(1:Ny,1+actualDataSets,2)=tmp(1:Ny,3); % values
-            case "DDS"
-                nAcq=nAcq+1;
-                fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
-                % check cycle prog, to guarantee continuity
-                tmp=split(files(iSet).name,"-");
-                tmpCyProg=str2num(tmp{3});
-                tmpCyCode=string(tmp{2});
-                if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
-                    % fast forward with NaNs
-                    [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
-                end
-                %
-                tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',1,'MultipleDelimsAsOne',true));
-                % x-axis values
-                measData(1:Nx,1,1)=tmp(1:Nx,1);                % fiber positions
-                measData(1:Nx,1+actualDataSets,1)=tmp(1:Nx,2); % values
-                % y-axis values
-                measData(1:Ny,1,2)=tmp(1:Ny,3);                % fiber positions
-                measData(1:Ny,1+actualDataSets,2)=tmp(1:Ny,4); % values
-            case "GIM"
-                if (strcmp(files(iSet).name,"Dati_SummaryGIM.txt") | startswith(files(iSet).name,"Int_","IgnoreCase",true) | startswith(files(iSet).name,"Profiles_","IgnoreCase",true))
-                    % matlab does not support single char wildcard...
-                    continue
-                end
-                nAcq=nAcq+1;
-                fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
-                % check cycle prog, to guarantee continuity
-                tmp=split(files(iSet).name,"_");
-                tmpCyProg=str2num(tmp{2});
-                tmpCyCode=string(tmp{3});
-                if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
-                    % fast forward with NaNs
-                    [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
-                end
-                %
-                tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',1,'MultipleDelimsAsOne',true));
-                % actual number of columns in file (ie frames+1)
-                nColumns=size(tmp,2);
-                % x-axis values
-                measData(1:Nx,1:nColumns,1,actualDataSets)=tmp(1:Nx,1:nColumns); % values
-                % y-axis values
-                measData(1:Ny,1:nColumns,2,actualDataSets)=tmp(Nx+2+1:Nx+2+Ny,1:nColumns); % values
-                % check cycle code
-                tmpCyCode=extractBetween(tmpCyCode,5,strlength(tmpCyCode));
-            case {"PMM","PIB"}
-                if (strfind(files(iSet).name,"Norm"))
-                    fprintf("   ...skipping Norm file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
-                    continue
-                end
-                nAcq=nAcq+1;
-                fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
-                % check cycle prog, to guarantee continuity
-                tmp=split(files(iSet).name,"-");
-                tmp=split(tmp{2},".");
-                tmpCyProg=str2num(tmp{1});
-                tmpCyCode=string(missing());
-                if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
-                    % fast forward with NaNs
-                    [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
-                end
-                %
-                tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'MultipleDelimsAsOne',true));
-                % x-axis values
-                measData(1:Nx,1:maxColumns,1,actualDataSets)=tmp(1:Nx,1:maxColumns); % values
-                % y-axis values
-                measData(1:Ny,1:maxColumns,2,actualDataSets)=tmp(1:Ny,1+2:2+maxColumns); % values
-            case {"QBM","SFH","SFM","SFP"}
-                nAcq=nAcq+1;
-                fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
-                % check cycle prog, to guarantee continuity
-                tmp=split(files(iSet).name,"-");
-                tmpCyProg=str2num(tmp{3});
-                tmpCyCode=string(tmp{2});
-                if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
-                    % fast forward with NaNs
-                    [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
-                end
-                %
-                tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',10,'MultipleDelimsAsOne',true));
-                % actual number of columns in file (ie frames+1)
-                nColumns=size(tmp,2);
-                % x-axis values
-                measData(1:Nx,1:nColumns,1,actualDataSets)=tmp(1:Nx,1:nColumns); % values
-                % y-axis values
-                measData(1:Ny,1:nColumns,2,actualDataSets)=tmp(Nx+2:Nx+1+Ny,1:nColumns); % values
-                % check cycle code
-                if ( strcmpi(fFormat,"SFP") || strcmpi(fFormat,"SFM") || strcmpi(fFormat,"QBM") )
-                    tmpCyCode=extractBetween(tmpCyCode,5,strlength(tmpCyCode));
-                end
+    for iPath=1:length(paths2Files)
+        
+        %% filename and extension
+        [filepath,name,ext]=fileparts(paths2Files(iPath));
+        if (strlength(name)==0)
+            switch upper(fFormat)
+                case "GIM"
+                    name="XY_*";
+                case "SFP"
+                    name="Data-*";
+                otherwise
+                    error("...please specify name for %s profile files",fFormat);
+            end
         end
-        % store cycle prog and cycle code
-        cyProgs(actualDataSets)=tmpCyProg;
-        cyCodes(actualDataSets)=tmpCyCode;
-        actualDataSets=actualDataSets+1;
+        if (strlength(ext)==0)
+            switch upper(fFormat)
+                case "GIM"
+                    ext=".txt";
+                case "SFP"
+                    ext=".csv";
+                otherwise
+                    error("...please specify extension for %s profile files",fFormat);
+            end
+        end
+
+        files=dir(strcat(filepath,"\",name,ext));
+        nDataSets=length(files);
+        fprintf("acquring %i data sets with %s format...\n",nDataSets,upper(fFormat));
+
+        nAcq=0;
+        for iSet=1:nDataSets
+            switch upper(fFormat)
+                case "CAM"
+                    nAcq=nAcq+1;
+                    fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
+                    % check cycle prog, to guarantee continuity
+                    tmp=split(files(iSet).name,"_");
+                    tmpCyProg=str2num(tmp{1});
+                    tmpCyCode=string(tmp{2});
+                    if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
+                        % fast forward with NaNs
+                        [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
+                    end
+                    %
+                    tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',1,'MultipleDelimsAsOne',true));
+                    % x-axis values
+                    measData(1:Nx,1,1)=tmp(1:Nx,1);                % fiber positions
+                    measData(1:Nx,1+actualDataSets,1)=tmp(1:Nx,2); % values
+                    % y-axis values
+                    measData(1:Ny,1,2)=tmp(1:Ny,1);                % fiber positions
+                    measData(1:Ny,1+actualDataSets,2)=tmp(1:Ny,3); % values
+                case "DDS"
+                    nAcq=nAcq+1;
+                    fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
+                    % check cycle prog, to guarantee continuity
+                    tmp=split(files(iSet).name,"-");
+                    tmpCyProg=str2num(tmp{3});
+                    tmpCyCode=string(tmp{2});
+                    if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
+                        % fast forward with NaNs
+                        [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
+                    end
+                    %
+                    tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',1,'MultipleDelimsAsOne',true));
+                    % x-axis values
+                    measData(1:Nx,1,1)=tmp(1:Nx,1);                % fiber positions
+                    measData(1:Nx,1+actualDataSets,1)=tmp(1:Nx,2); % values
+                    % y-axis values
+                    measData(1:Ny,1,2)=tmp(1:Ny,3);                % fiber positions
+                    measData(1:Ny,1+actualDataSets,2)=tmp(1:Ny,4); % values
+                case "GIM"
+                    if (strcmp(files(iSet).name,"Dati_SummaryGIM.txt") | startswith(files(iSet).name,"Int_","IgnoreCase",true) | startswith(files(iSet).name,"Profiles_","IgnoreCase",true))
+                        % matlab does not support single char wildcard...
+                        continue
+                    end
+                    nAcq=nAcq+1;
+                    fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
+                    % check cycle prog, to guarantee continuity
+                    tmp=split(files(iSet).name,"_");
+                    tmpCyProg=str2num(tmp{2});
+                    tmpCyCode=string(tmp{3});
+                    if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
+                        % fast forward with NaNs
+                        [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
+                    end
+                    %
+                    tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',1,'MultipleDelimsAsOne',true));
+                    % actual number of columns in file (ie frames+1)
+                    nColumns=size(tmp,2);
+                    % x-axis values
+                    measData(1:Nx,1:nColumns,1,actualDataSets)=tmp(1:Nx,1:nColumns); % values
+                    % y-axis values
+                    measData(1:Ny,1:nColumns,2,actualDataSets)=tmp(Nx+2+1:Nx+2+Ny,1:nColumns); % values
+                    % check cycle code
+                    tmpCyCode=extractBetween(tmpCyCode,5,strlength(tmpCyCode));
+                case {"PMM","PIB"}
+                    if (strfind(files(iSet).name,"Norm"))
+                        fprintf("   ...skipping Norm file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
+                        continue
+                    end
+                    nAcq=nAcq+1;
+                    fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
+                    % check cycle prog, to guarantee continuity
+                    tmp=split(files(iSet).name,"-");
+                    tmp=split(tmp{2},".");
+                    tmpCyProg=str2num(tmp{1});
+                    tmpCyCode=string(missing());
+                    if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
+                        % fast forward with NaNs
+                        [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
+                    end
+                    %
+                    tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'MultipleDelimsAsOne',true));
+                    % x-axis values
+                    measData(1:Nx,1:maxColumns,1,actualDataSets)=tmp(1:Nx,1:maxColumns); % values
+                    % y-axis values
+                    measData(1:Ny,1:maxColumns,2,actualDataSets)=tmp(1:Ny,1+2:2+maxColumns); % values
+                case {"QBM","SFH","SFM","SFP"}
+                    nAcq=nAcq+1;
+                    fprintf("...parsing file %d/%d: %s ...\n",iSet,nDataSets,files(iSet).name);
+                    % check cycle prog, to guarantee continuity
+                    tmp=split(files(iSet).name,"-");
+                    tmpCyProg=str2num(tmp{3});
+                    tmpCyCode=string(tmp{2});
+                    if ( actualDataSets>1 && tmpCyProg>cyProgs(actualDataSets-1)+1 && nAcq>0 )
+                        % fast forward with NaNs
+                        [measData,cyProgs,cyCodes,actualDataSets]=FastForwardProfileAcquisitions(measData,cyProgs,cyCodes,actualDataSets,tmpCyProg,cyProgs(actualDataSets-1));
+                    end
+                    %
+                    tmp=table2array(readtable(sprintf("%s\\%s",files(iSet).folder,files(iSet).name),'HeaderLines',10,'MultipleDelimsAsOne',true));
+                    % actual number of columns in file (ie frames+1)
+                    nColumns=size(tmp,2);
+                    % x-axis values
+                    measData(1:Nx,1:nColumns,1,actualDataSets)=tmp(1:Nx,1:nColumns); % values
+                    % y-axis values
+                    measData(1:Ny,1:nColumns,2,actualDataSets)=tmp(Nx+2:Nx+1+Ny,1:nColumns); % values
+                    % check cycle code
+                    if ( strcmpi(fFormat,"SFP") || strcmpi(fFormat,"SFM") || strcmpi(fFormat,"QBM") )
+                        tmpCyCode=extractBetween(tmpCyCode,5,strlength(tmpCyCode));
+                    end
+            end
+            % store cycle prog and cycle code
+            cyProgs(actualDataSets)=tmpCyProg;
+            cyCodes(actualDataSets)=tmpCyCode;
+            actualDataSets=actualDataSets+1;
+        end
+        fprintf("...acqured %i files;\n",nAcq);
     end
-    fprintf("...acqured %i files;\n",nAcq);
     
     %% post-processing
     nAcquired=size(measData,4);
