e5baf04927de16e8d37d82d24809dad65807a46b
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-02-23 11:10:48 +0100

    Minor updates to operations/LGENvisualCheck.m: * better palette; * possibility to filter on (HEBT) families; * filtering on magnet names (no longer PS/LGEN names); * showing plots in order of magnet name (no longer PS/LGEN names);

--- operations/LGENvisualCheck.m
+++ operations/LGENvisualCheck.m
@@ -1,12 +1,26 @@
-function LGENvisualCheck(psNames,xVals,xLab,yVals,yLab,magNames,myTitle,myLegend,LGENs)
+function LGENvisualCheck(psNames,xVals,xLab,yVals,yLab,magNames,myTitle,myLegend,magNames2show)
     if (~exist("myTitle","var") || strlength(myTitle)==0 ), myTitle=missing(); end
     if (~exist("myLegend","var")), myLegend=missing(); end
-    if (~exist("LGENs","var")), LGENs=unique(psNames); end % default: do not filter
-    nMagnets=size(LGENs,1);
-    nSets=size(psNames,2);
+    if (exist("magNames2show","var"))
+        switch upper(magNames2show)
+            case {"QUE","QUES","QUADRUPOLES"}
+                magNames2show=unique(magNames(contains(magNames,"QUE")));
+                if (~ismissing(myTitle)), myTitle=strcat(myTitle," - only QUEs"); end
+            case {"CEB","CEBS","CORRECTORS"}
+                magNames2show=unique(magNames(contains(magNames,"CEB")));
+                if (~ismissing(myTitle)), myTitle=strcat(myTitle," - only CEBs"); end
+            case {"DIP","DIPS","DIPOLES"}
+                magNames2show=unique(magNames(contains(magNames,"SWH")|contains(magNames,"MBS")|contains(magNames,"SW2")));
+                if (~ismissing(myTitle)), myTitle=strcat(myTitle," - only MAIN DIPOLEs"); end
+        end
+    else
+        magNames2show=unique(magNames(~ismissing(magNames))); % default: do not filter
+    end
+    nMagnets=size(magNames2show,1);
+    nSets=size(magNames,2);
 
     figure();
-    cm=colormap(parula(nSets+1));
+    cm=colormap(turbo(nSets));
     % - set grid of plots
     nPlots=nMagnets;
     if (~ismissing(myLegend)), nPlots=nPlots+1; end
@@ -23,9 +37,9 @@ function LGENvisualCheck(psNames,xVals,xLab,yVals,yLab,magNames,myTitle,myLegend
         subplot(nRows,nCols,iMagnet);
         lFirst=true;
         for iSet=1:nSets
-            jj=find(strcmp(psNames(:,iSet),LGENs(iMagnet)));
+            jj=find(strcmp(magNames(:,iSet),magNames2show(iMagnet)));
             if ( isempty(jj) )
-                warning("...unable to find LGEN %s in list of PS names!",LGENs(iMagnet));
+                warning("...unable to find LGEN %s in list of PS names!",magNames2show(iMagnet));
                 continue
             end
             if (lFirst), lFirst=false; else, hold on; end
@@ -33,7 +47,7 @@ function LGENvisualCheck(psNames,xVals,xLab,yVals,yLab,magNames,myTitle,myLegend
             plot(xVals(IDs,iSet),yVals(IDs,jj,iSet),"-","Marker",markers(iSet),"Color",cm(iSet,:));
         end
         xlabel(xLab); ylabel(yLab); grid on;
-        title(sprintf("%s (aka %s)",magNames(jj,iSet),LGENs(iMagnet)));
+        title(sprintf("%s (aka %s)",magNames2show(iMagnet),psNames(jj,iSet)));
     end
     % - legend plot
     if (~ismissing(myLegend))
