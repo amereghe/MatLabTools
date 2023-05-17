12bf812319cb0193fa77307b3820ef8402be8113
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-05-17 12:31:50 +0200

    general/Plot2DHistograms.m returns axis objects
    At the same time, more robust checking of missing ellypses contouring distributions

--- general/Plot2DHistograms.m
+++ general/Plot2DHistograms.m
@@ -1,4 +1,4 @@
-function Plot2DHistograms(showMe,showMe1DX,showMe1DY,xShow,yShow,xShowLabel,yShowLabel,contours,lHist,lSquared)
+function [axM,axX,axY]=Plot2DHistograms(showMe,showMe1DX,showMe1DY,xShow,yShow,xShowLabel,yShowLabel,contours,lHist,lSquared)
     if ( ~exist("lHist","var") ), lHist=true; end
     if ( ~exist("lSquared","var") ), lSquared=false; end
     
@@ -11,7 +11,7 @@ function Plot2DHistograms(showMe,showMe1DX,showMe1DY,xShow,yShow,xShowLabel,ySho
         plot(showMe(:,1),showMe(:,2),"k.");
     end
     if ( exist("contours","var") )
-        if ( ~ismissing(contours) )
+        if ( ~all(ismissing(contours),"all") )
             for ii=1:size(contours,3)
                 hold on; plot(contours(:,1,ii),contours(:,2,ii),".-");
             end
@@ -26,7 +26,7 @@ function Plot2DHistograms(showMe,showMe1DX,showMe1DY,xShow,yShow,xShowLabel,ySho
     if ( lSquared )
         myMin=min(min(xShow),min(yShow)); myMax=max(max(xShow),max(yShow));
         if ( exist("contours","var") )
-            if ( ~ismissing(contours) )
+            if ( ~all(ismissing(contours),"all") )
                 myMin=min(myMin,min(contours,[],"all"));
                 myMax=max(myMax,max(contours,[],"all"));
             end
