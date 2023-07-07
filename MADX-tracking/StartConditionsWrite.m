40fde7f5328c5d81f5d3da06d982723c035f5bd4
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-07-07 16:53:42 +0200

    Improving numerical format of START condnitions

--- MADX-tracking/StartConditionsWrite.m
+++ MADX-tracking/StartConditionsWrite.m
@@ -1,7 +1,7 @@
 function StartConditionsWrite(FileName,particles,tracker)
     if ( ~exist('tracker','var') ), tracker="MADX"; end
     fprintf("writing starting conditions of %d particles to file %s (%s format)...\n",length(particles),FileName,tracker);
-    myFmt=StartConditionsFormat(tracker);
+    myFmt=StartConditionsFormat(tracker,"W");
     fileID = fopen(FileName,'w');
     for jj=1:length(particles)
         fprintf(fileID,"%s\n",sprintf(myFmt,particles(jj,:)));
