63dfca2e2546e9dd83a634229fad367f178568bc
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-08-01 16:15:42 +0200

    filtering on observation point when parsing dump files by MADX/PTC

--- MADX-tracking/ReadSurvivors.m
+++ MADX-tracking/ReadSurvivors.m
@@ -1,4 +1,4 @@
-function [ particles, obsPoints, nParts ] = ReadSurvivors(path2Files)
+function [ particles, obsPoints, nParts ] = ReadSurvivors(path2Files,obsPointNames)
 % READSURVIVORS  Read survivors from MAD-X/PTC tracking.
 %
 % example format of line of parsed files (argument of ID is read as well):
@@ -14,10 +14,12 @@ function [ particles, obsPoints, nParts ] = ReadSurvivors(path2Files)
 %       the third dimension identifies the obervation point;
 %   obsPoints: list of names of the obervation points;
 
+    %% set up
+    if (~exist("obsPointNames","var")), obsPointNames=missing(); end
     [ colNames, colUnits, colFacts, mapping, readFormat ] = ...
         GetColumnsAndMappingTFS("track");
     
-    % acquire files
+    %% acquire files
     particles=missing(); obsPoints=missing(); nParts=missing();
     for kk=1:length(path2Files)
         files=dir(path2Files(kk));
@@ -39,23 +41,27 @@ function [ particles, obsPoints, nParts ] = ReadSurvivors(path2Files)
                 elseif (startsWith(tLine,"#"))
                     % new section
                     myFields=split(tLine);
-                    nParts=ExpandMat(nParts,double(string(myFields{4})));
-                    clear tmpParticles;
-                    tmpParticles=cell2mat(textscan(fileID,readFormat));
-                    fprintf("...acquired %d particles at %s;\n",nParts(end),myFields{end});
-                    % add closed orbit
-                    if (strcmpi(myFields{end},"START"))
-                        for jj=1:length(CO6Dnames)
-                            if (CO6D(jj)~=0.0)
-                                iCol=mapping(strcmpi(colNames,extractBetween(CO6Dnames(ii),1,strlength(CO6Dnames(ii))-1)));
-                                tmpParticles(:,iCol)=tmpParticles(:,iCol)+CO6D(jj);
-                                fprintf("...taking into account 6D closed orbit on %s at START;\n",colNames(iCol));
+                    if ( all(ismissing(obsPointNames)) | ~isempty(find(contains(obsPointNames,myFields{end}))) )
+                        % acquire beam population at the specified obs point
+                        %    or at every obs point
+                        nParts=ExpandMat(nParts,double(string(myFields{4})));
+                        clear tmpParticles;
+                        tmpParticles=cell2mat(textscan(fileID,readFormat));
+                        fprintf("...acquired %d particles at %s;\n",nParts(end),myFields{end});
+                        % add closed orbit
+                        if (strcmpi(myFields{end},"START"))
+                            for jj=1:length(CO6Dnames)
+                                if (CO6D(jj)~=0.0)
+                                    iCol=mapping(strcmpi(colNames,extractBetween(CO6Dnames(ii),1,strlength(CO6Dnames(ii))-1)));
+                                    tmpParticles(:,iCol)=tmpParticles(:,iCol)+CO6D(jj);
+                                    fprintf("...taking into account 6D closed orbit on %s at START;\n",colNames(iCol));
+                                end
                             end
                         end
+                        % store in memory
+                        particles=ExpandMat(particles,tmpParticles);
+                        obsPoints=ExpandMat(obsPoints,string(myFields{end}));
                     end
-                    % store in memory
-                    particles=ExpandMat(particles,tmpParticles);
-                    obsPoints=ExpandMat(obsPoints,string(myFields{end}));
                 end
             end
             fclose(fileID);
@@ -63,26 +69,29 @@ function [ particles, obsPoints, nParts ] = ReadSurvivors(path2Files)
     end
     fprintf("...done with parsing files;\n");
     
-    % compact storage
-    uObsPoints=unique(obsPoints);
-    if (length(uObsPoints)~=length(obsPoints))
-        fprintf("...compacting from %d data sets to %d (unique obs points);\n",length(obsPoints),length(uObsPoints));
-        readParticles=particles; readObsPoints=obsPoints; readNparts=nParts;
-        clear particles obsPoints nParts;
-        particles=missing(); obsPoints=missing(); nParts=missing();
-        for ii=1:length(uObsPoints)
-            iObs=find(strcmp(readObsPoints,uObsPoints(ii)));
-            clear tmpParticles;
-            nParts=ExpandMat(nParts,sum(readNparts(iObs)));
-            tmpParticles=NaN(nParts(end),length(colNames));
-            jRead=0;
-            for jj=1:length(iObs)
-                tmpParticles(jRead+1:jRead+readNparts(iObs(jj)),:)=readParticles(1:readNparts(iObs(jj)),:,iObs(jj));
-                jRead=jRead+readNparts(iObs(jj));
+    %% compact storage
+    %  only in case all observation points are parsed
+    if ( all(ismissing(obsPointNames)) )
+        uObsPoints=unique(obsPoints);
+        if (length(uObsPoints)~=length(obsPoints))
+            fprintf("...compacting from %d data sets to %d (unique obs points);\n",length(obsPoints),length(uObsPoints));
+            readParticles=particles; readObsPoints=obsPoints; readNparts=nParts;
+            clear particles obsPoints nParts;
+            particles=missing(); obsPoints=missing(); nParts=missing();
+            for ii=1:length(uObsPoints)
+                iObs=find(strcmp(readObsPoints,uObsPoints(ii)));
+                clear tmpParticles;
+                nParts=ExpandMat(nParts,sum(readNparts(iObs)));
+                tmpParticles=NaN(nParts(end),length(colNames));
+                jRead=0;
+                for jj=1:length(iObs)
+                    tmpParticles(jRead+1:jRead+readNparts(iObs(jj)),:)=readParticles(1:readNparts(iObs(jj)),:,iObs(jj));
+                    jRead=jRead+readNparts(iObs(jj));
+                end
+                particles=ExpandMat(particles,tmpParticles);
+                obsPoints=ExpandMat(obsPoints,uObsPoints(ii));
             end
-            particles=ExpandMat(particles,tmpParticles);
-            obsPoints=ExpandMat(obsPoints,uObsPoints(ii));
+            fprintf("...done with compacting data storage;\n");
         end
-        fprintf("...done with compacting data storage;\n");
     end
 end
