90a2cf8bf5c61cee65c25a647caeb2259aafd2bb
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-08-03 16:09:45 +0200

    units can be passed through interface of BeamStats.m function

--- optics/BeamStats.m
+++ optics/BeamStats.m
@@ -1,4 +1,4 @@
-function [Counts2D,CountsX,CountsY,xb,yb,contours,contourLabels]=BeamStats(particles,what2Analise,declared,xbQuery,ybQuery,contours,contourLabels,myTitle)
+function [Counts2D,CountsX,CountsY,xb,yb,contours,contourLabels]=BeamStats(particles,what2Analise,declared,units,xbQuery,ybQuery,contours,contourLabels,myTitle)
 % 
 % particles=float(nParticles,2*nPlanes);
 % particle coordinates expressed in physical units, i.e. [m] and [rad]!
@@ -6,6 +6,7 @@ function [Counts2D,CountsX,CountsY,xb,yb,contours,contourLabels]=BeamStats(parti
     %% interface vars
     if (~exist("what2Analise","var") | all(ismissing(what2Analise)) ), what2Analise=["X" "PX" ; "Y" "PY" ]; end
     if (~exist("declared","var") | all(ismissing(declared)) ), declared=["X" "PX" "Y" "PY" "T" "PT"]; end
+    if (~exist("units","var") | all(ismissing(units))), units=["m" "" "m" "" "m" ""];  end
     if (~exist("xbQuery","var") | all(ismissing(xbQuery)) ), xbQuery=NaN(1,round(size(particles,2))/2); end % histogram bins set based on data
     if (~exist("ybQuery","var") | all(ismissing(ybQuery)) ), ybQuery=NaN(1,round(size(particles,2))/2); end % histogram bins set based on data
     if (~exist("contours","var") | ismissing(contours)), contours=missing(); end
@@ -33,9 +34,9 @@ function [Counts2D,CountsX,CountsY,xb,yb,contours,contourLabels]=BeamStats(parti
     %% plot
     if (~ismissing(myTitle))
         %      2D histogram
-        lHist=true;  BeamStatsShow(Counts2D,CountsX,CountsY,xb,yb,what2Analise,lHist,myTitle,contours,contourLabels,missing(),declared);
+        lHist=true;  BeamStatsShow(Counts2D,CountsX,CountsY,xb,yb,what2Analise,lHist,myTitle,contours,contourLabels,units,declared);
         %      scatter plot
-        lHist=false; BeamStatsShow(particles,CountsX,CountsY,xb,yb,what2Analise,lHist,myTitle,contours,contourLabels,missing(),declared);
+        lHist=false; BeamStatsShow(particles,CountsX,CountsY,xb,yb,what2Analise,lHist,myTitle,contours,contourLabels,units,declared);
     end
     
 end
